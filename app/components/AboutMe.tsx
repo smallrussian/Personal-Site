1d365fdd1c9a2f6fe9b07c99096dfe7b6c7dfa7d
smallrussian <julian.shniter@gmail.com>
2023-05-29 18:17:43 -0500

    fixed linting and removed useless component

--- app/components/AboutMe.tsx
+++
@@ -1,9 +0,0 @@
-const AboutMe = () => {
-    return (
-        <div className="about-me my-10">
-          <h2 className="text-2xl font-bold mb-2">About Me</h2>
-          <p className="text-lg">Since the first time I picked up a chess piece, age 4, I have always enjoyed problem solving.</p>
-        </div>
-           
-      );
-}
